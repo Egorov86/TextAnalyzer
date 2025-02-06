1214bf4faf1bc26179614f1e386c576a6dc92c0a
Egorov E.A. <104271590+Egorov86@users.noreply.github.com>
2025-02-06 19:28:00 +0300

    Add project files.

---
+++ TextAnalyzer/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace TextAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.analyzeButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.reportTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(374, 22);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // analyzeButton
+            // 
+            this.analyzeButton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.analyzeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.analyzeButton.Location = new System.Drawing.Point(12, 70);
+            this.analyzeButton.Name = "analyzeButton";
+            this.analyzeButton.Size = new System.Drawing.Size(153, 41);
+            this.analyzeButton.TabIndex = 1;
+            this.analyzeButton.Text = "Запуск анализа";
+            this.analyzeButton.UseVisualStyleBackColor = false;
+            this.analyzeButton.Click += new System.EventHandler(this.analyzeButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelButton.Location = new System.Drawing.Point(219, 71);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(167, 41);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Остановка анализа";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // reportTextBox
+            // 
+            this.reportTextBox.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.reportTextBox.Location = new System.Drawing.Point(12, 142);
+            this.reportTextBox.Multiline = true;
+            this.reportTextBox.Name = "reportTextBox";
+            this.reportTextBox.Size = new System.Drawing.Size(374, 114);
+            this.reportTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 272);
+            this.Controls.Add(this.reportTextBox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.analyzeButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button analyzeButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TextBox reportTextBox;
+    }
+}
+
---
+++ TextAnalyzer/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Использую async для выполнения длительной операции анализа текста в фоновом потоке.
+        private async void analyzeButton_Click(object sender, EventArgs e)
+        {
+            // Инициализация токена отмены
+            _cancellationTokenSource = new CancellationTokenSource();
+            var token = _cancellationTokenSource.Token;
+
+            try
+            {
+                // Запуск анализа текста
+                var report = await Task.Run(() => AnalyzeText(inputTextBox.Text, token), token);
+                reportTextBox.Text = report;
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("Анализ был отменен.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}");
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            // Отмена анализа
+            _cancellationTokenSource?.Cancel();
+        }
+        private string AnalyzeText(string text, CancellationToken token)
+        {
+            // Проверка на отмену
+            token.ThrowIfCancellationRequested();
+
+            int sentenceCount = 0;
+            int wordCount = 0;
+            int characterCount = text.Length;
+            int questionCount = 0;
+            int exclamationCount = 0;
+
+            // Разделение текста на предложения
+            var sentences = text.Split(new[] {'.','!','?'}, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var sentence in sentences)
+            {
+                // Проверка на отмену
+                token.ThrowIfCancellationRequested();
+
+                // Увеличиваем счетчик предложений
+                sentenceCount++;
+
+                // Подсчет слов
+                var words = sentence.Split(new[] {' ','\n','\r'}, StringSplitOptions.RemoveEmptyEntries);
+                wordCount += words.Length;
+
+                // Проверяем, какое это предложение
+                string trimmedSentence = sentence.Trim();
+                if (trimmedSentence.EndsWith("?"))
+                    {
+                        questionCount++;
+                    }
+
+                if (trimmedSentence.EndsWith("!"))
+                    {
+                        exclamationCount++;
+                    }
+            }
+            /*
+            foreach (var sentence in sentences)
+            {
+                // Проверка на отмену
+                token.ThrowIfCancellationRequested();
+
+                if (sentence.EndsWith("?"))
+                    {
+                        questionCount++;
+                    }
+
+                if (sentence.EndsWith("!"))
+                    {
+                        exclamationCount++;
+                    } 
+
+                // Подсчет слов
+                var words = sentence.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                wordCount += words.Length;
+            }*/
+
+            // Формирование отчета
+            StringBuilder reportBuilder = new StringBuilder();
+            reportBuilder.AppendLine($"Количество предложений: {sentenceCount}");
+            reportBuilder.AppendLine($"Количество символов: {characterCount}");
+            reportBuilder.AppendLine($"Количество слов: {wordCount}");
+            reportBuilder.AppendLine($"Количество вопросительных предложений: {questionCount}");
+            reportBuilder.AppendLine($"Количество восклицательных предложений: {exclamationCount}");
+
+            return reportBuilder.ToString();
+        }
+    }
+}
